9f25ccc7b5f7a46104cf333122bd56df623003e7
Sam Markham <sam.e.markham@gmail.com>
2020-04-15 15:13:15 -0700

    Reorganizes novel model to fix rails console errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   has_many :novels
   has_secure_password
 
-  validates :username, :name, presence: true
+  validates :username, presence: true
+  validates :name, presence: true
   validates :username, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 5, wrong_length: "Password must be at least 5 characters." }
 end
