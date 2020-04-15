90a9ca2fe84faa587e9c8659a8998bf1a0eed071
Sam Markham <sam.e.markham@gmail.com>
2020-04-15 14:20:53 -0700

    Adds methods to novel controller

--- app/controllers/api/v1/novels_controller.rb
+++ app/controllers/api/v1/novels_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::NovelsController < ApplicationController
+
+  def index
+    novels = Novel.all
+    render json: novels
+  end
+
+  def show
+    novel = Novel.find(params[:id])
+    render json: novel
+    # render json: TrainerSerializer.new(trainer).to_serialized_json, status: 200
+  end
+
+  def create
+    novel = Novel.new(novel_params)
+    if novel.save
+      render json: novel
+    else
+      render json: novel.errors, status: :unprocessable_entity
+    # render json: TrainerSerializer.new(trainer).to_serialized_json, status: 200
+  end
+
+  def update
+    novel = Novel.find(params[:id])
+    if novel.update(novel_params)
+      render json: novel
+      # render json: PokemonSerializer.new(novel).to_serialized_json, status: 200
+    else
+      render json: { status: 500 }
+    end
+  end
+
+  private
+
+  def novel_params
+    params.require(:novel).permit(:id, :title, :summary, :plan, :goal, :wordcount, :user_id)
+  end
+
 end
