e877455da7d46711c1249d4497482fc336bfd619
Sam Markham <sam.e.markham@gmail.com>
2020-04-15 12:25:48 -0700

    Adds validations to models

--- app/models/novel.rb
+++ app/models/novel.rb
@@ -1,4 +1,7 @@
 class Novel < ApplicationRecord
   belongs_to :user
   has_many :badges
+
+  validates :title, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :novels
+  has_secure_password
+
+  validates :username, :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 5, wrong_length: "Password must be at least 5 characters." }
 end
