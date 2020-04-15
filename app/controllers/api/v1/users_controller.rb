6b8894293bee4c29d562133c31a2475470159910
Sam Markham <sam.e.markham@gmail.com>
2020-04-15 13:21:40 -0700

    Starts building out users_controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,33 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
+
+  def index
+    users = User.all
+    render json: users
+  end
+
+  # def show
+  #   trainer = Trainer.find(params[:id])
+  #   render json: TrainerSerializer.new(trainer).to_serialized_json, status: 200
+  # end
+
+  # def create
+  #   trainer = Trainer.create(trainer_params)
+  #   render json: TrainerSerializer.new(trainer).to_serialized_json, status: 200
+  # end
+
+  # def update
+  #   @trainer = Trainer.find(params[:id])
+  #   if @trainer.update(trainer_params)
+  #     render json: TrainerSerializer.new(@trainer).to_serialized_json, status: 200
+  #   else
+  #     render json: { status: 500 }
+  #   end
+  # end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :username, :email, :password)
+  end
+
 end
