a03aa4b0a6f684d4a7df0e0cfe80654604078639
Sam Markham <sam.e.markham@gmail.com>
2020-04-15 13:24:50 -0700

    Changes user params on users controller to reflect updated attributes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -27,7 +27,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:id, :username, :email, :password)
+    params.require(:user).permit(:id, :name, :username, :password)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -38,8 +38,8 @@ ActiveRecord::Schema.define(version: 2020_04_15_190556) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "username"
-    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
