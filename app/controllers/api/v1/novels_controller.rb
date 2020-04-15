ec3ed15bc1c62f0e4d7cd3dd80b6fc67eb5313d3
Sam Markham <sam.e.markham@gmail.com>
2020-04-15 15:05:00 -0700

    Finishes adding novel serializer to novels controller

--- app/controllers/api/v1/novels_controller.rb
+++ app/controllers/api/v1/novels_controller.rb
@@ -8,14 +8,14 @@ class Api::V1::NovelsController < ApplicationController
 
   def show
     novel = Novel.find(params[:id])
-    render json: novel
+    render json: NovelSerializer.new(novel).to_serialized_json, status: 200
     # render json: TrainerSerializer.new(trainer).to_serialized_json, status: 200
   end
 
   def create
     novel = Novel.new(novel_params)
     if novel.save
-      render json: novel
+      render json: NovelSerializer.new(novel).to_serialized_json, status: 200
     else
       render json: novel.errors, status: :unprocessable_entity
     # render json: TrainerSerializer.new(trainer).to_serialized_json, status: 200
@@ -25,7 +25,7 @@ class Api::V1::NovelsController < ApplicationController
   def update
     novel = Novel.find(params[:id])
     if novel.update(novel_params)
-      render json: novel
+      render json: NovelSerializer.new(novel).to_serialized_json, status: 200
       # render json: PokemonSerializer.new(novel).to_serialized_json, status: 200
     else
       render json: { status: 500 }
