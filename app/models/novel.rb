7054e2117164c1450c6fa77177baae44ed11b214
Sam Markham <sam.e.markham@gmail.com>
2020-04-17 15:48:50 -0700

    Adds code to novel model that sets wordcount to start at 0 if no value is entered for it.

--- app/models/novel.rb
+++ app/models/novel.rb
@@ -4,4 +4,9 @@ class Novel < ApplicationRecord
 
   validates :title, presence: true
 
+  before_save :default_values
+
+  def default_values
+    self.wordcount ||= 0 # note self.status = 'P' if self.status.nil? might be safer (per @frontendbeauty)
+  end
 end
