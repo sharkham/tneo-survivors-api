42767265fb789e1e5d37829823d98c53cb2171b6
Sam Markham <sam.e.markham@gmail.com>
2020-04-17 12:20:06 -0700

    Adds logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   post '/api/v1/login' => "api/v1/sessions#create"
+  delete '/api/v1/logout' => "api/v1/sessions#destroy"
+
   get '/api/v1/get_current_user' => "api/v1/sessions#get_current_user"
 
   namespace :api do
