b0c1b9062e4299978c8c5a10ca1161f07fb7b578
Sam Markham <sam.e.markham@gmail.com>
2020-04-17 15:52:24 -0700

    Adds code to novel model so year is set to current year on initialization

--- app/models/novel.rb
+++ app/models/novel.rb
@@ -8,5 +8,6 @@ class Novel < ApplicationRecord
 
   def default_values
     self.wordcount ||= 0 # note self.status = 'P' if self.status.nil? might be safer (per @frontendbeauty)
+    self.year ||= Date.current.year
   end
 end
