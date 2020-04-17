9dfb2d754061b2c41f2bcb73c585d57ddf9ebe80
Sam Markham <sam.e.markham@gmail.com>
2020-04-16 17:39:11 -0700

    Adds get_current_user route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   post '/api/v1/login' => "api/v1/sessions#create"
+  get '/api/v1/get_current_user' => "api/v1/sessions#get_current_user"
 
   namespace :api do
     namespace :v1 do
