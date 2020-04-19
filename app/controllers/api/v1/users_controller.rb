c493e1a6b3f6b123ed7773321bfe9d02fdc38758
Sam Markham <sam.e.markham@gmail.com>
2020-04-19 14:51:16 -0700

    Adds fuller error response to users#new action in users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,23 +13,27 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+    byebug
     if user.save
       render json: user
     else
-      render json: user.errors, status: :unprocessable_entity
+      resp = {
+        error: user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
     # render json: TrainerSerializer.new(trainer).to_serialized_json, status: 200
     end
   end
 
-  def update
-    user = User.find(params[:id])
-    if user.update(user_params)
-      render json: user
-      # render json: PokemonSerializer.new(novel).to_serialized_json, status: 200
-    else
-      render json: { status: 500 }
-    end
-  end
+  # def update
+  #   user = User.find(params[:id])
+  #   if user.update(user_params)
+  #     render json: user
+  #     # render json: PokemonSerializer.new(novel).to_serialized_json, status: 200
+  #   else
+  #     render json: { status: 500 }
+  #   end
+  # end
 
   private
 
