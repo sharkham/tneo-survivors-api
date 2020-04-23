7aeaaa253e6468336e6ef02984f7b62644894176
Sam Markham <sam.e.markham@gmail.com>
2020-04-23 13:16:58 -0700

    Sessions controller now uses UserSerializer so password isn't displayed

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::SessionsController < ApplicationController
     @user = User.find_by(username: params[:session][:username])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      render json: @user
+      render json: UserSerializer.new(@user).to_serialized_json, status: 200
     else
       render json: {
         error: "Invalid Credentials"
@@ -14,7 +14,8 @@ class Api::V1::SessionsController < ApplicationController
 
   def get_current_user
     if logged_in?
-      render json: current_user
+      render json: UserSerializer.new(current_user).to_serialized_json, status: 200
+      # render json: current_user
     # else
     #   render json: {
     #     notice: "No one logged in"
