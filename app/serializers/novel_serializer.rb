4bdab0f890b087d7998f5311c838e3cc5f492c1b
Sam Markham <sam.e.markham@gmail.com>
2020-04-23 13:14:32 -0700

    Adjusts novel_serializer so user's name is included in novel json object

--- app/serializers/novel_serializer.rb
+++ app/serializers/novel_serializer.rb
@@ -7,6 +7,9 @@ class NovelSerializer
   def to_serialized_json
     options = {
       include: {
+        user: {
+          only: :name
+        },
         badges: {
           only: [:id, :name, :description, :emoji, :novel_id, :badgetype_id]
         }
