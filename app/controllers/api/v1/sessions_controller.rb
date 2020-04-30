e61b254afec9d342e6b532179630f1175452511d
Sam Markham <sam.e.markham@gmail.com>
2020-04-30 13:45:05 -0700

    Changes username search in sessions controller to email search

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
 
   def create
-    @user = User.find_by(username: params[:session][:username])
+    @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       render json: UserSerializer.new(@user).to_serialized_json, status: 200
