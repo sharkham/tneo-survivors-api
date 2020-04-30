131cb0dff13523c1a47ae03ab9dd7175dca247af
Sam Markham <sam.e.markham@gmail.com>
2020-04-30 13:39:24 -0700

    Removes username from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   has_many :novels
   has_secure_password
 
-  validates :username, :email, :name, presence: true
+  validates :email, :name, presence: true
   # validates :name, presence: true
-  validates :username, uniqueness: { case_sensitive: false }
+  # validates :username, uniqueness: { case_sensitive: false }
   validates :email, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 5, wrong_length: "Password must be at least 5 characters." }
 end
