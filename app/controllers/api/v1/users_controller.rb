f93f0103f3151981939b79f289945cc4156eb811
Sam Markham <sam.e.markham@gmail.com>
2020-04-30 13:43:28 -0700

    Changes username to email on user_params in users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -38,7 +38,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:id, :name, :username, :password)
+    params.require(:user).permit(:id, :name, :email, :password)
   end
 
 end
