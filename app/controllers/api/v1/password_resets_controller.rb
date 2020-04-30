7e323fd7fa05b5f0c7fa3f9a90c4cbb02a096c96
Sam Markham <sam.e.markham@gmail.com>
2020-04-30 15:53:19 -0700

    Comments out user.send_password_reset method for testing pu rposes

--- app/controllers/api/v1/password_resets_controller.rb
+++ app/controllers/api/v1/password_resets_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::PasswordResetsController < ApplicationController
     user = User.find_by(email: params[:email])
     byebug
     if user
-      user.send_password_reset
+      # user.send_password_reset
       #just for testing
       alert: "User exists"
     end
