53e863e4acaa550b17e17076464a21999331110c
Sam Markham <sam.e.markham@gmail.com>
2020-04-30 13:34:29 -0700

    Adds email attribute to user migration

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_04_15_221508) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "email"
     t.string "username"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
