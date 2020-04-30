9ecc8801ad366d841e6208496fd8051a050bb083
Sam Markham <sam.e.markham@gmail.com>
2020-04-30 14:01:36 -0700

    Adds create method to password resets controller

--- app/controllers/api/v1/password_resets_controller.rb
+++ app/controllers/api/v1/password_resets_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::PasswordResetsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user
+      user.send_password_reset
+      #just for testing
+      alert: "User exists"
+    end
+    #this sends regardless of whether there's an email in database for security reasons
+    alert: "Email sent with password reset instructions."
+  end
 end
