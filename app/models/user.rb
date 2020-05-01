4070c97f62e05af1ed0135328b16550d4c8acca4
Sam Markham <sam.e.markham@gmail.com>
2020-05-01 11:55:37 -0700

    Adds send_password_reset method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  include UserHelpers
   has_many :novels
   has_secure_password
 
@@ -7,4 +8,13 @@ class User < ApplicationRecord
   # validates :username, uniqueness: { case_sensitive: false }
   validates :email, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 5, wrong_length: "Password must be at least 5 characters." }
+
+  def send_password_reset
+    UserHelpers.generate_base64_token
+    #   self.password_reset_token = UserHelpers.generate_base64_token # plain method lives in its own file
+    #   self.password_reset_sent_at = Time.zone.now
+    #   save!
+    # UserMailer.password_reset(self).deliver
+  end
+
 end
