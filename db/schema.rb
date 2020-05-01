88f00ba6e566e0df960389bb21c48aa5c7e3bdec
Sam Markham <sam.e.markham@gmail.com>
2020-05-01 11:20:17 -0700

    Adds password_reset_token column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_221508) do
+ActiveRecord::Schema.define(version: 2020_05_01_181934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2020_04_15_221508) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
   add_foreign_key "badges", "novels"
