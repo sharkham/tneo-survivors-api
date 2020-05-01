124dcf581c26d4cc56faf70735e30a79366e1d17
Sam Markham <sam.e.markham@gmail.com>
2020-05-01 12:53:52 -0700

    Adds password_reset method to user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,16 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.password_reset.subject
   #
-  def password_reset
-    @greeting = "Hi"
+  # def password_reset
+  #   @greeting = "Hi"
 
-    mail to: "to@example.org"
+  #   mail to: "to@example.org"
+  # end
+
+  default from: "from@example.com"
+
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password Reset"
   end
 end
