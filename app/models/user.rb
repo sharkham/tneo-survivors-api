6da12779c7667db026c854a4c49778119301fb78
Sam Markham <sam.e.markham@gmail.com>
2020-05-01 13:14:25 -0700

    Adds conditional to password validations in user model so validations won't fire if password isn't being set

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,16 +9,16 @@ class User < ApplicationRecord
   # validates :name, presence: true
   # validates :username, uniqueness: { case_sensitive: false }
   validates :email, uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 5, wrong_length: "Password must be at least 5 characters." }
+  validates :password, length: { minimum: 5, wrong_length: "Password must be at least 5 characters." }, if: :password
 
   def send_password_reset
     # UserHelpers.generate_base64_token
     self.password_reset_token = generate_base64_token
     #   self.password_reset_token = UserHelpers.generate_base64_token # plain method lives in its own file
     self.password_reset_sent_at = Time.zone.now
-    byebug
-    # save!
-    # UserMailer.password_reset(self).deliver
+    # byebug
+    save!
+    UserMailer.password_reset(self).deliver
   end
 
   private
