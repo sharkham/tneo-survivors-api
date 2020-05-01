f2b2df9f1b3d827416e2f003062035cf422c6bc4
Sam Markham <sam.e.markham@gmail.com>
2020-05-01 14:29:17 -0700

    Organizes development file settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,10 +33,13 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  #added settings
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: GMAIL_ACCOUNT}
+
+  #specific gmail settings
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
