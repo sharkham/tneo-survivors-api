ddfa88db68649b635b962e437747a03e8d5831d0
Sam Markham <sam.e.markham@gmail.com>
2020-04-30 17:14:56 -0700

    Fixes create method so alert fires if user is found

--- app/controllers/api/v1/password_resets_controller.rb
+++ app/controllers/api/v1/password_resets_controller.rb
@@ -1,13 +1,17 @@
 class Api::V1::PasswordResetsController < ApplicationController
   def create
-    user = User.find_by(email: params[:email])
-    byebug
+    user = User.find_by(email: params[:_json])
+    # byebug
     if user
       # user.send_password_reset
       #just for testing
-      alert: "User exists"
+      render json: {
+        alert: "User exists!"
+      }
     end
     #this sends regardless of whether there's an email in database for security reasons
-    alert: "Email sent with password reset instructions."
+    # render json: {
+    #   alert: "Email sent with password reset instructions."
+    # }
   end
 end
