c0c6c57b9905e4c5c5978c448f7ea118433ebca7
Sam Markham <sam.e.markham@gmail.com>
2020-05-01 18:30:55 -0700

    Adds reset action to passwords controller

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -22,4 +22,24 @@ class Api::V1::PasswordsController < ApplicationController
   def reset
     byebug
   end
+
+  # def reset
+  #   token = params[:token].to_s
+
+  #   if params[:email].blank?
+  #     return render json: {error: 'Token not present'}
+  #   end
+
+  #   user = User.find_by(reset_password_token: token)
+
+  #   if user.present? && user.password_token_valid?
+  #     if user.reset_password!(params[:password])
+  #       render json: {status: 'ok'}, status: :ok
+  #     else
+  #       render json: {error: user.errors.full_messages}, status: :unprocessable_entity
+  #     end
+  #   else
+  #     render json: {error:  ['Link not valid or expired. Try generating a new link.']}, status: :not_found
+  #   end
+  # end
 end
