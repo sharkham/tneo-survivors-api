94ef6e431c960de848ac3df7c6b702986aa93f6c
Sam Markham <sam.e.markham@gmail.com>
2020-05-06 11:43:58 -0700

    Cleans up passwords_controller

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -1,31 +1,24 @@
 class Api::V1::PasswordsController < ApplicationController
   def forgot
     user = User.find_by(email: params[:_json])
-    # byebug
     if user
       render json: {
         alert: "If this user exists, we have sent you a password reset email."
       }
       user.send_password_reset
-      #just for testing
     else
+      #this sends regardless of whether there's an email in database for security reasons
       render json: {
         alert: "If this user exists, we have sent you a password reset email."
       }
     end
-    #this sends regardless of whether there's an email in database for security reasons
-    # render json: {
-    #   alert: "Email sent with password reset instructions."
-    # }
   end
 
   def reset
-    # byebug
     user = User.find_by(password_reset_token: params[:token], email: params[:email])
 
     if user.present? && user.password_token_valid?
       if user.reset_password(params[:password])
-        # render json: user, status: 200
         render json: {
           alert: "Your password has been successfuly reset!"
         }
@@ -38,23 +31,5 @@ class Api::V1::PasswordsController < ApplicationController
     end
   end
 
-  # def reset
-  #   token = params[:token].to_s
 
-  #   if params[:email].blank?
-  #     return render json: {error: 'Token not present'}
-  #   end
-
-  #   user = User.find_by(password_reset_token: token)
-
-  #   if user.present? && user.password_token_valid?
-  #     if user.reset_password!(params[:password])
-  #       render json: {status: 'ok'}, status: :ok
-  #     else
-  #       render json: {error: user.errors.full_messages}, status: :unprocessable_entity
-  #     end
-  #   else
-  #     render json: {error:  ['Link not valid or expired. Try generating a new link.']}, status: :not_found
-  #   end
-  # end
 end
