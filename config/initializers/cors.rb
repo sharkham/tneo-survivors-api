565fad47dd13d780e63202a30c7d65a1914cb28a
Sam Markham <sam.e.markham@gmail.com>
2020-05-07 12:17:34 -0700

    Changes cors settings to test where error in setting origins is

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['FRONT_END_URL']
+    # origins ENV['FRONT_END_URL']
+    origins 'https://novel-tracker-app.netlify.app/'
 
     resource '*',
       headers: :any,
